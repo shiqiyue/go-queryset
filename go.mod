ac9fdef80aeb3f7f2ac8f06373b858fbf4cbf2a4
wwy <835802084@qq.com>
2022-12-20 14:53:04 +0800

    升级依赖

--- go.mod
+++ go.mod
@@ -3,13 +3,14 @@ module github.com/shiqiyue/go-queryset
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/iancoleman/strcase v0.2.0
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/lib/pq v1.10.4
 	github.com/pkg/errors v0.8.1
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/tools v0.0.0-20200103221440-774c71fcf114
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
-	gorm.io/gorm v1.22.5
+	gorm.io/gorm v1.24.2
 )
 
 go 1.13
--- models.go
+++ models.go
@@ -1,21 +1,23 @@
 package go_queryset
 
 import (
+	"gorm.io/gorm"
 	"time"
 )
 
 //go:generate goqueryset -in $GOFILE
 // gen:qs
-// 素材标签使用统计
-type IdeaTagStat struct {
-	Id          int64     `gorm:"primaryKey;comment:主键"`
-	CreatedAt   time.Time `gorm:"not null;comment:创建时间"`
-	UpdatedAt   time.Time `gorm:"not null;comment:更新时间"`
-	TagId       int64     `gorm:"not null;comment:标签ID"`
-	UseCount    int64     `gorm:"not null; comment:使用次数"`
-	SearchCount int64     `gorm:"not null; comment:搜索次数"`
+// Contact 联系人
+type Contact struct {
+	ID        int64          `gorm:"primarykey; comment:ID"`
+	CreatedAt time.Time      `gorm:"not null; comment:创建时间"`
+	UpdatedAt time.Time      `gorm:"not null; comment:更新时间"`
+	DeletedAt gorm.DeletedAt `gorm:"index; comment:删除时间"`
+	Name      string         `gorm:"not null; size:255; comment:名称"`
+	Email     string         `gorm:"not null; size:255; comment:邮箱"`
+	Phone     string         `gorm:"not null; size: 255; comment:手机号码"`
 }
 
-func (s *IdeaTagStat) TableName() string {
-	return "irm_idea_tag_stat"
+func (p *Contact) TableName() string {
+	return "nc_contact"
 }
